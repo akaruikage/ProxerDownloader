a9d7c2f34c008d70b5e88ffd637a575484aa5e03
Akarui Kage <akaruikage.ak@gmail.com>
2017-10-15 13:55:23 +0200

    Exception, thrown when no proxer stream is found

--- ma/akaruikage/proxer/downloader/NoProxerStreamFoundException.java
+++ ma/akaruikage/proxer/downloader/NoProxerStreamFoundException.java
@@ -2,9 +2,6 @@ package ma.akaruikage.proxer.downloader;
 
 public class NoProxerStreamFoundException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public NoProxerStreamFoundException() {
