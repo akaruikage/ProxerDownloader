42a3df5ebf961397bafc3ec7e700694dff6868ef
Akarui Kage <akaruikage.ak@gmail.com>
2017-10-15 12:49:12 +0200

    main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package ma.akaruikage.proxer.downloader;
+
+public class Main {
+
+	static String s = "";
+
+	public static void main(String[] args) throws Exception {
+		Frame f = new Frame();
+		f.getRiddOfWarningXD();
+	}
+
+}
